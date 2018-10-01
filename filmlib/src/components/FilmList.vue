18fb16cfaf5bd4f1664f487c4c1c02098f4bc539
fess932 <fess932@gmail.com>
2018-10-01 07:43:22 +0300

    фыва

--- filmlib/src/components/FilmList.vue
+++ filmlib/src/components/FilmList.vue
@@ -2,26 +2,30 @@
     <div class="filmlist container-fluid">
         <ul class="list-group">
             <li class="list-group-item" v-for="(film, key) in films" :key="key">
-              <img @click="open(film.img)" :src="film.img" class="rounded img-fluid float-left" width="60vh" alt="...">
+              <img @click="open(film)" :src="film.img" class="rounded img-fluid float-left" width="60vh" alt="...">
               <h5>{{film.label}}</h5>
-
-              <sweet-modal ref="modal">
-               Success!
-              </sweet-modal>
-
             </li>
         </ul>
         <pre> {{$data}} </pre>
 
-        <sweet-modal ref="filmImage">
-          <img :src="filmImage" alt="">
+        <sweet-modal  modal-theme="dark" overlay-theme="dark" ref="filmImage">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-sm">
+                <img class="rounded" width="100%" :src="filmImage" alt="">
+              </div>
+              <div class="col-sm">
+                <div> {{ filmInfo }} </div>
+              </div>
+            </div>
+          </div>          
         </sweet-modal>
     </div>
 </template>
 
 <script>
 import { SweetModal, SweetModalTab } from 'sweet-modal-vue'
-import films from '@/content.json'
+import DataBase from '@/content.json'
 export default {
   components: {
     SweetModal,
@@ -30,13 +34,17 @@ export default {
   name: 'FilmList',
   data () {
     return {
-      films: films,
-      filmImage: ""
+      films: DataBase.films,
+      filmImage: '',
+      filmInfo: '',
+      filmLabel: ''
     }
   },
   methods: {
-    open: function (img) {
-      this.filmImage = img
+    open: function (film) {
+      this.filmImage = film.img
+      this.filmInfo = film.info
+      this.filmLabel = film.label
       this.$refs.filmImage.open()
     }
   }
