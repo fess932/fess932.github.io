cccb02b865bf66fa11597a002360296d62d62aba
fess932 <fess932@gmail.com>
2018-10-02 20:10:31 +0300

    sdf

--- vuetifyfilm/src/main.js
+++ vuetifyfilm/src/main.js
@@ -6,11 +6,24 @@ import router from './router'
 import store from './store'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import * as fb from 'firebase/app'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created () {
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyDPof-Q_nAVPjt-QU9z38xOcEP05JXpfPM',
+      authDomain: 'list-c582d.firebaseapp.com',
+      databaseURL: 'https://list-c582d.firebaseio.com',
+      projectId: 'list-c582d',
+      storageBucket: 'list-c582d.appspot.com',
+      messagingSenderId: '644236981443'
+    }
+    fb.initializeApp(config)
+  },
   render: h => h(App)
 }).$mount('#app')
