e0a6b172e861f92a90a528313ecfb3456199bc7f
fess932 <fess932@gmail.com>
2018-10-12 10:59:10 +0300

    sd

---
+++ filmyoutube/src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+// import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: Registration
+    }
+  ]
+})
