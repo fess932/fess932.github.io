9b7d939a0cf910b97a272360d99d41f65816c7c8
fess932 <fess932@gmail.com>
2018-10-13 08:06:44 +0300

    try remove from arry

--- filmyoutube/src/components/Home.vue
+++ filmyoutube/src/components/Home.vue
@@ -247,8 +247,16 @@ export default {
           title: tag.title
         })
       } else {
+
+        ////////////////////// TODO
+        _.remove(this.tagsUsed, tag => tag.title )
         // _.pull(this.tagsUsed, tag)
-        this.tagsUsed.splice(tag.title, 1)
+        // this.tagsUsed.splice(tag.title, 1)
+        // _.remove(this.tagsUsed, function (item) {
+        //   return item.title === this.tagsUsed
+        // });
+        // _.remove(this.tagsUsed, {tag})
+        console.log(tag)
       }
     },
     getHoursAndMinutes(minutes) {
