5c980ee2a77a7555b6e68cce8689a2bbe8e15c2e
fess932 <fess932@gmail.com>
2018-10-17 08:04:52 +0300

    asdf

--- filmyoutube/src/main.js
+++ filmyoutube/src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import lodash from 'lodash'
 
 import router from './router'
 import store from './store'
 
+Vue.use(Vuelidate)
+
 Object.defineProperty(Vue.prototype, '$lodash', { value: lodash })
+
+
 
 Vue.config.productionTip = false
 
