f1b549653d0b190c24da69924280fdfedbbde7d4
fess932 <fess932@gmail.com>
2018-10-29 07:51:16 +0300

    ыва

--- filmyoutube/src/main.js
+++ filmyoutube/src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import Uimini from 'uimini/dist/css/uimini.css'
-import App from './App.vue'
+
 import lodash from 'lodash'
 
+import App from './App'
 import router from './router'
 import store from './store'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/messaging'
+import 'firebase/storage'
 
 Vue.use(Vuelidate, Uimini)
 
@@ -16,5 +23,16 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    var config = {
+      apiKey: 'AIzaSyBvO2XRE3NiFIp47b74ygtnjcRJ5Dd3Fdc',
+      authDomain: 'films-d7557.firebaseapp.com',
+      databaseURL: 'https://films-d7557.firebaseio.com',
+      projectId: 'films-d7557',
+      storageBucket: 'films-d7557.appspot.com',
+      messagingSenderId: '323432055398'
+    }
+    firebase.initializeApp(config)
+  }
 }).$mount('#app')
